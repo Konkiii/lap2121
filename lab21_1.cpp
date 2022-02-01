cf460cb25f9b08119d9112d78e9940df61ec4c9d
Konkiii <lovegangajaa99@gmail.com>
2022-02-01 17:12:11 +0700

    add ans

--- lab21_1.cpp
+++ lab21_1.cpp
@@ -5,8 +5,15 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect R1, Rect R2){
+	double minx = min(R1.x + R1.w, R2.x + R2.w);
+	double maxx = max(R1.x, R2.x);
+	double miny = min(R1.y, R2.y);
+	double maxy = max(R1.x - R1.h, R2.x - R2.h);
+	double overlapx = minx - maxx;
+	double overlapy = miny - maxy;
+	if(overlapx <= 0 || overlapy <= 0){
+		return 0;
+	}
+	return overlapx * overlapy;
 }
